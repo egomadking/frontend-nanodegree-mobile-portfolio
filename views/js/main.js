184616f040bc1a515d5ed821b055e571d54b39ff
egomadking <ego.madking@gmail.com>
2017-12-02 20:51:03 -0500

    simplified sizeSwitcher, changePizzaSizes. Put pizza motion in callback function for rAF. Used rAF in DOM event listener

--- views/js/main.js
+++ views/js/main.js
@@ -417,41 +417,29 @@ var resizePizzas = function(size) {
     }
   }
 
-  changeSliderLabel(size);
-
-   // Returns the size difference to change a pizza element from one size to another. Called by changePizzaSlices(size).
-  function determineDx (elem, size) {
-    var oldWidth = elem.offsetWidth;
-    var windowWidth = document.querySelector("#randomPizzas").offsetWidth;
-    var oldSize = oldWidth / windowWidth;
-
-    // Changes the slider value to a percent width
-    function sizeSwitcher (size) {
-      switch(size) {
-        case "1":
-          return 0.25;
-        case "2":
-          return 0.3333;
-        case "3":
-          return 0.5;
-        default:
-          console.log("bug in sizeSwitcher");
-      }
+  // Looks at the switch postion and returns a value
+  // that can be used to be applied to 
+  // .randomPizzaContainer's style. Consider moving to
+  // bootstrap grid to take advantage of breakpoints
+  
+  function sizeSwitcher (size) {
+    switch(size) {
+      case "1":
+        return 25;
+      case "2":
+        return 33.33;
+      case "3":
+        return 50;
+      default:
+        console.log("bug in sizeSwitcher");
     }
-
-    var newSize = sizeSwitcher(size);
-    var dx = (newSize - oldSize) * windowWidth;
-
-    return dx;
   }
-  // end pizza size slider
-
-  // Iterates through pizza elements on the page and changes their widths
+  
+  // size switcher evaluates the the switch position
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var pizzaContainerAmt = document.querySelectorAll('.randomPizzaContainer').length;
+    for (var i = 0; i < pizzaContainerAmt; i++) {
+      document.querySelectorAll('.randomPizzaContainer')[i].style.width = sizeSwitcher(size) + '%';
     }
   }
 
@@ -500,19 +488,18 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  //
-  //
-  // The for() loop is burining the most time
-  // pay attention to items and scrollTop
-  //
-  //  
   var items = document.querySelectorAll('.mover');
-  for (var i = 0; i < items.length; i++) {
-    // document.body.scrollTop is no longer supported in Chrome.
-    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    var phase = Math.sin((scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+  // document.body.scrollTop is no longer supported in Chrome.
+  var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+
+  
+  function pizzaMath() {
+    for (var i = 0; i < items.length; i++) {
+      var phase = Math.sin((scrollTop / 1250) + (i % 5));
+      items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    }
   }
+  requestAnimationFrame(pizzaMath);
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
   // Super easy to create custom metrics.
@@ -531,7 +518,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+  for (var i = 0; i < 56; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
@@ -541,7 +528,8 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
   }
-  updatePositions();
+  requestAnimationFrame(updatePositions);
+  //updatePositions();
 });
 
 (function(){
